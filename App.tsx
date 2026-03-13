b7448a29de952193fe492a7472047714926020ce
luisaludewig <267018830+luisaludewig@users.noreply.github.com>
2026-03-13 14:06:16 +0100

    Import Router component from wouter

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
